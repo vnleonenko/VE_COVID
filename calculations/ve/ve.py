00a1fda2fa3414003a5be060795062626a52d385
Kseniya Sahatova <kseniya.shtv.98@gmail.com>
2022-10-07 17:10:11 +0300

    GLM is added for ve and ci estimation

--- calculations/ve/ve.py
+++ calculations/ve/ve.py
@@ -1,6 +1,11 @@
+import numpy as np
 import pandas as pd
-from utils import connect_to_db, query_to_df, add_aggregated_data, group_by_subjects
-from pcv_ppv import compute_ppv, compute_pcv
+from tqdm import tqdm
+
+from .utils import connect_to_db, query_to_df, add_aggregated_data, group_by_subjects
+from .pcv_ppv import compute_ppv
+import statsmodels.formula.api as smf
+import statsmodels.api as sm
 
 
 def compute_ve(pcv, ppv):
@@ -22,10 +27,58 @@ def compute_ve(pcv, ppv):
             else:
                 age = '_total'
             case = ve_column.split('_')[1]
-            factor_pcv = merged_pcv_ppv['pcv_'+case+age] / (1 - merged_pcv_ppv['pcv_'+case+age])
-            factor_ppv = (1 - merged_pcv_ppv['ppv'+age]) / merged_pcv_ppv['ppv'+age]
-            ve_df[ve_column] = 1 - (factor_pcv * factor_ppv)
+            factor_pcv = merged_pcv_ppv['pcv_' + case + age] / (1 - merged_pcv_ppv['pcv_' + case + age])
+            factor_ppv = (1 - merged_pcv_ppv['ppv' + age]) / merged_pcv_ppv['ppv' + age]
+            ve_df[ve_column] = round(1 - (factor_pcv * factor_ppv), 5)
 
+    return ve_df
+
+
+def compute_ve_conf_int(inf_vac_data, ppv_data):
+    def interim_df(args):
+        # print(args)
+        case, vac_case, ppv = int(args[0]), int(args[1]), args[2]
+        df = pd.DataFrame(columns=['case', 'vac_case', 'ppv'])
+        df['y'] = [1 if i <= vac_case else 0 for i in range(case)]
+        if ppv == 0:
+            return np.nan, np.nan, np.nan
+
+        df['logit_ppv'] = np.log(ppv / (1 - ppv))
+        df = df.fillna({'case': case, 'vac_case': vac_case, 'ppv': ppv})
+        glm = smf.glm(formula='y ~ 1', data=df, offset=df['logit_ppv'],
+                      family=sm.families.Binomial()).fit()
+        ve_estimation = round(1 - np.exp(glm.params).values[0], 5)
+        ci = [round(ci, 5) for ci in 1 - np.exp(glm.conf_int(alpha=0.05)).values[0]]
+        print(ve_estimation, *ci)
+        return ve_estimation, ci[1], ci[0]
+
+    merged_df = ppv_data.merge(inf_vac_data, on=['data_point', 'region', 'vac_interval_group', 'vaccine'], how='outer')
+    # nans_sum1 = merged_df.isnull().sum().sum()
+    merged_df.replace([np.inf, -np.inf, np.nan], 0, inplace=True)
+
+    merged_df.iloc[:, :4] = merged_df.iloc[:, :4].astype('category')
+    merged_df.iloc[:, 4:7] = merged_df.iloc[:, 4:7].astype('float32')
+    merged_df.iloc[:, 7:] = merged_df.iloc[:, 7:].astype('int32')
+
+    merged_df_cut = merged_df.iloc[:100, :]
+
+    ve_df = merged_df_cut[['data_point', 'region', 'vac_interval_group', 'vaccine']]
+    case_age = ['zab_18_59', 'hosp_18_59', 'severe_18_59', 'death_18_59',
+                'zab_60', 'hosp_60', 'severe_60', 'death_60',
+                'zab_total', 'hosp_total', 'severe_total', 'death_total']
+
+    for ca in tqdm(case_age):
+        inf_column = 'count_' + ca
+        inf_vac_column = 'count_vac_' + ca
+        ppv_column = 'ppv_' + "_".join(ca.split('_')[1:])
+        ve_column = 've_' + ca
+        cil_column = 'cil_' + ca
+        cih_column = 'cih_' + ca
+        ve_df[[ve_column, cil_column, cih_column]] = \
+            merged_df_cut[[inf_column, inf_vac_column, ppv_column]].apply(lambda x: interim_df(x) if np.all(x[:2]) != 0
+        else [np.nan, np.nan, np.nan], axis=1, result_type="expand")
+
+    ve_df.to_csv(f'calculations/output/ve_w_ci_test.csv', sep=';', index=False, encoding='cp1251', na_rep='NULL')
     return ve_df
 
 
@@ -156,7 +209,7 @@ def main():
     pop_df = query_to_df(pop_q, db_cursor, pop_columns)
 
     inf_vac_grouped_df = add_aggregated_data(inf_vac_df, ['data_point', 'vac_interval_group', 'vaccine'],
-                                                         ['data_point', 'region', 'vac_interval_group'])
+                                             ['data_point', 'region', 'vac_interval_group'])
     inf_grouped_df = group_by_subjects(inf_df, ['data_point'])
     inf_grouped_df = pd.concat([inf_df, inf_grouped_df])
     inf_grouped_df.sort_values(by=['data_point', 'region'], inplace=True)
@@ -168,17 +221,24 @@ def main():
     vac_grouped_df = add_aggregated_data(vac_df, ['data_point', 'vac_interval_group', 'vaccine'],
                                          ['data_point', 'region', 'vac_interval_group'])
     vac_pop_grouped_df = vac_grouped_df.merge(pop_df, on='region', how='left')
-
-    pcv_df = compute_pcv(inf_vac_grouped_df)
     ppv_df = compute_ppv(vac_pop_grouped_df)
+    '''
+    vac_pop_grouped_df.to_csv('calculations/output/vac_pop.csv', sep=';', index=False, encoding='cp1251', na_rep='NULL')
+    inf_vac_grouped_df.to_csv('calculations/output/inf_vac.csv', sep=';', index=False, encoding='cp1251', na_rep='NULL')
+    ppv_df.to_csv('calculations/output/ppv.csv', sep=';', index=False, encoding='cp1251', na_rep='NULL')
+    inf_vac_grouped_cut_df.to_csv('calculations/output/inf_vac_cut.csv', sep=';', index=False, encoding='cp1251',
+                                  na_rep='NULL')
+    '''
 
-    ve = compute_ve(pcv_df, ppv_df)
-    ve.to_csv('calculations/output/ve.csv', sep=';', index=False, encoding='cp1251')
+    # pcv_df = compute_pcv(inf_vac_grouped_df)
+    # ve = compute_ve(pcv_df, ppv_df)
+
+    inf_vac_grouped_cut_df = inf_vac_grouped_df.loc[inf_vac_grouped_df['data_point'] == '2022.07_16-08-2022']
+    ppv_cut_df = ppv_df.loc[ppv_df['data_point'] == '2022.07_16-08-2022']  # '2021.11_01-07-2022'
+
+    ve_conf_int = compute_ve_conf_int(inf_vac_grouped_cut_df, ppv_cut_df)
+    ve_conf_int.to_csv('calculations/output/ve_w_ci.csv', sep=';', index=False, encoding='cp1251', na_rep='NULL')
 
 
 if __name__ == '__main__':
     main()
-
-
-
-
